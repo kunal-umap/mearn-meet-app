aa577b445958221e63c37b30f6734b7d17c220f0
Kunal Umap <80446986+kunal-umap@users.noreply.github.com>
2022-01-26 16:40:42 +0530

    heroku setup

--- server/server.js
+++ server/server.js
@@ -49,6 +49,13 @@ io.on("connection", (socket) => {
 	});
 });
 
+if(process.env.NODE_ENV == "production"){
+	app.use(express.static("build"));
+	const path = require("path");
+	app.get("*",(req,res)=>{
+		res.sendFile(path.resolve(__dirname,'build','index.html'));
+	})
+}
 
 server.listen(PORT, ()=>{
     console.log(`Srever is running on http://localhost:${PORT}`)
